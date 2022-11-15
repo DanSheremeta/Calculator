93b1c8d02d83d2b03e7a0c1aa695db1d4cf109ed
DanSheremeta <danylo3002@gmail.com>
2022-11-15 14:10:42 +0200

    Добавьте файлы проекта.

---
+++ AnalizerClassLibrary.Tests/AnalaizerClassTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalaizerClassLibrary.Tests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"D:\danylo\Ed\University\3 курс\Тести\Lab N1\Calculator\TestData.xml", "data", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void CreateStack_FromDataSourceTest()
+        {
+            // Arrange
+            string initial = (string)TestContext.DataRow["initial"];
+            string expected = (string)TestContext.DataRow["expected"];
+            AnalaizerClass.expression = initial;
+
+            // Actual
+            string actual = string.Join("", AnalaizerClass.CreateStack().ToArray());
+
+            // Assert
+            Assert.AreEqual(expected, actual);  
+        }
+    }
+}
